abeaca29c8f5a5dc1f8591301b96e89fb5be8214
ureshaLakshani <ureshalak97@gmail.com>
2022-08-25 21:49:07 +0530

    initial commit

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Uresha Lakshani on 2022-08-25.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
